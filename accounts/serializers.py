05464a262398544659da3ad5702c16931bcd77ce
Gabriel Bessi <tecbessi@gmail.com>
2023-02-25 19:05:45 -0300

    feat: Criação do app accounts

---
+++ accounts/serializers.py
@@ -0,0 +1,47 @@
+from rest_framework import serializers
+from .models import User
+from rest_framework.validators import UniqueValidator
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from django.contrib.auth.hashers import make_password
+
+
+class UserSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    username = serializers.CharField(
+        max_length=150,
+        validators=[UniqueValidator(queryset=User.objects.all(), message="username already taken.")])
+    password = serializers.CharField(max_length=127, write_only=True)
+    email = serializers.EmailField(
+        max_length=127,
+        validators=[UniqueValidator(queryset=User.objects.all(), message="email already registered.")])
+    birthdate = serializers.DateField(required=False, allow_null=True)
+    first_name = serializers.CharField(max_length=50)
+    last_name = serializers.CharField(max_length=50)
+    is_employee = serializers.BooleanField(required=False)
+    is_superuser = serializers.BooleanField(read_only=True)
+    
+    def update(self, instance: User, validated_data: dict):
+        for key, value in validated_data.items():
+            if key == 'password':
+                value = make_password(value)
+            setattr(instance, key, value)
+
+        instance.save()
+
+        return instance
+
+    def create(self, validated_data: dict):
+        if "is_employee" in validated_data and validated_data["is_employee"]:
+            return User.objects.create_superuser(**validated_data)
+        else:
+            return User.objects.create_user(**validated_data)
+
+
+
+class CustomJWTSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token["is_superuser"] = user.is_superuser
+
+        return token
---
+++ accounts/views.py
@@ -0,0 +1,44 @@
+from rest_framework.views import APIView, Request, Response, status
+from .serializers import UserSerializer, CustomJWTSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated
+from .permissions import UsersDetailsPermission
+from django.shortcuts import get_object_or_404
+from .models import User
+
+
+class UsersView(APIView):
+    def post(self, request: Request) -> Response:
+        serializer = UserSerializer(data=request.data)
+
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status.HTTP_201_CREATED)
+
+
+class UsersDetailView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated, UsersDetailsPermission]
+
+    def get(self, request: Request, user_id: int) -> Response:
+        get_user = get_object_or_404(User, id=user_id)
+        self.check_object_permissions(request, get_user)
+
+        serializer = UserSerializer(get_user)
+        return Response(serializer.data, status.HTTP_200_OK)
+
+    def patch(self, request: Request, user_id: int) -> Response:
+        update_user = get_object_or_404(User, id=user_id)
+        self.check_object_permissions(request, update_user)
+
+        serializer = UserSerializer(update_user, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+
+        serializer.save()
+
+        return Response(serializer.data, status.HTTP_200_OK)
+
+class UsersLoginView(TokenObtainPairView):
+    serializer_class = CustomJWTSerializer
